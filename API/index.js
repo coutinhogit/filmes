d2efba5da75cb8793e6030f288414edb28a4d7ac
Gabriel Coutinho Cezar <gabrielcoutinho.cezar@gmail.com>
2025-12-23 18:42:46 -0300

    adicionando cors na api

--- API/index.js
+++ API/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const csv = require('csv-parser');
 const fs = require('fs');
+const cors = require('cors');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use = (cors());
+app.use(express.json());
+
 let movies = [];
 
 fs.createReadStream('imdb_top_1000.csv')
